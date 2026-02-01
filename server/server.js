e4b8834594486ff94593fea6a9bc0af883ae3838
Naman Chaudhary <namanvit2028@gmail.com>
2026-02-02 01:07:42 +0530

    Initialized backend server skeleton

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+const app = express();
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+// import uploadRoutes from "./routes/uploadRoutes.js";
+// import queryRoutes from "./routes/queryRoutes.js";
+
+
+app.get("/",(req,res)=>{
+    res.send("MemoryMap API is running...");
+});
+
+app.use("/api/upload", uploadRoutes);
+app.use("/api/query", queryRoutes);
+
+const port = 8080;
+
+app.listen(port, ()=>{
+    console.log(`App is listening on port : ${port}`);
+});
